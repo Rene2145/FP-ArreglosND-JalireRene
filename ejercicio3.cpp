a36bdf3021f0ec3d3f206361f27ee10e79fedee8
Rene David Charaña Jalire <darkreingreso@gmail.com>
2025-07-06 16:34:29 -0500

    Version final, Suma de las diagonales de una matriz(4x4)

--- ejercicio3.cpp
+++ ejercicio3.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 using namespace std;
 int main(){
+	cout << "Ejercicio 3: Suma de las diagonales de una matriz(4x4)\n\n";
 	int m1[4][4];
 	int sd1=0;
 	int sd2=0;
-	
+
+	cout << "Ingresa los datos de la matriz:\n\n";
 	cout << "1) Matriz A" << endl;
 	for(int i=0;i<4;i++){
 		for(int j=0;j<4;j++){
-			cout << "  [" << i << "][" << j << "] = ";
+			cout <<"  [" << i << "][" << j << "] = ";
             cin >>m1[i][j];
 		}
 	}
@@ -16,7 +18,7 @@ int main(){
 	
 	cout << "\n2) Matriz A\n\n";
 	for(int i=0;i<4;i++){
-		cout << "  ";
+		cout << " ";
 		for(int j=0;j<4;j++){
 		cout<<" "<<m1[i][j]<<" ";
 		}
@@ -30,7 +32,9 @@ int main(){
 
 
 	cout<<"\nResultado:\n";
-	cout<<"La suma de las dos diagonales es "<<sd1+sd2;
+	cout<<"  \n* La suma de la primera diagonal es "<<sd1;
+	cout<<"  \n* La suma de la segunda diagonal es "<<sd2;
+	cout<<"  \n* La suma de las dos diagonales es "<<sd1+sd2;
 
 	return 0;
 }
