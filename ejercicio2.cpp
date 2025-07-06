fa01bdc804cc7bed2feeb08b85051328b074214f
Rene David Charaña Jalire <darkreingreso@gmail.com>
2025-07-06 14:54:09 -0500

    Version final, Buscando un Valor en la Matriz(3x3)

--- ejercicio2.cpp
+++ ejercicio2.cpp
@@ -1,23 +1,27 @@
 #include <iostream>
 using namespace std;
 int main(){
+	cout << "Ejercicio 2: Buscar un Valor en una Matriz(3x3)\n\n";
 	int m1[3][3];
 	int v;
-	cout << "Ingresa los datos de la matriz(3x3):\n\n";
-	cout << "1) Matriz (3x3)" << endl;
+	cout << "Ingresa los datos de la matriz:\n\n";
+	cout << "1) Matriz A" << endl;
 	for(int i=0;i<3;i++){
 		for(int j=0;j<3;j++){
 			cout << "  [" << i << "][" << j << "] = ";
             cin >>m1[i][j];
 		}
 	}
+	
+	cout << "\n2) Matriz A\n";
 	for(int i=0;i<3;i++){
 		cout << "  ";
 		for(int j=0;j<3;j++){
-			cout<<" "<<m1[i][j]<<" ";
+		cout<<" "<<m1[i][j]<<" ";
 		}
+		cout<<endl;
 	}
-	cout << "\nValor a buscar: ";
+	cout << "\nValor que deseas buscar: ";
     cin >> v;
     
     cout << "\nEl valor " << v << " se encuentra en:\n";
