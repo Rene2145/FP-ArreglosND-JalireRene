f3abbc2084d3cc7a9f0c8303d1d473cddea0346e
Rene David Charaña Jalire <darkreingreso@gmail.com>
2025-07-06 13:11:45 -0500

    Implementación de suma de dos matrices(3x3)

--- ejercicio1.cpp
+++ ejercicio1.cpp
@@ -1,32 +1,35 @@
 #include <iostream>
 using namespace std;
 int main(){
-	cout << "Ejercicio 1: Multiplicacion de dos matrices\n\n";
-	int m1[3][3]={
-		{1,2,3},
-		{4,5,6},
-		{7,8,9}
-	};
-	int m2[3][3]={
-		{9,8,7},
-		{6,5,4},
-		{3,2,1}
-	};
-	int p[3][3]={
-		{0,0,0},
-		{0,0,0},
-		{0,0,0}
-	};
+	cout << "Ejercicio 1: Multiplicacion de dos matrices(3x3)\n\n";
+	int m1[3][3];
+	int m2[3][3];
+	int p[3][3];
 	
+	cout << "Matriz - Multiplicando:" << endl;
 	for(int i=0;i<3;i++){
 		for(int j=0;j<3;j++){
-			for (int k=0;k<3;k++){ 
-                p[i][j]+=m1[i][k]*m2[k][j];
-            }
+			cout << "[" << i << "][" << j << "] = ";
+            cin >>m1[i][j];
 		}
 	}
 	
-	cout<<"Multplicando\t\tMultiplicador\t\t  Producto\n\n";
+	cout << "\nMatriz - Multiplicador:" << endl;
+	for(int i=0;i<3;i++){
+		for(int j=0;j<3;j++){
+			cout << "[" << i << "][" << j << "] = ";
+            cin >>m2[i][j];
+		}
+	}
+
+		for(int i=0;i<3;i++){
+			for(int j=0;j<3;j++){
+            p[i][j] = m1[i][j] + m2[i][j];
+		}
+	}
+	
+	cout<<"\nRESULTADO:\n\n";
+	cout<<"Multiplicando\t\tMultiplicador\t\t  Producto\n\n";
 	for(int i=0;i<3;i++){
 		for(int j=0;j<3;j++){
 			cout<<" "<<m1[i][j]<<" ";
@@ -39,8 +42,7 @@ int main(){
 		for(int j=0;j<3;j++){
 			cout<<" "<<p[i][j]<<" ";
 		}
-		cout<<"\t\t";
-		cout<<endl;
+		cout<<"\t\t"<<endl;
 	}
 	
 	return 0;
