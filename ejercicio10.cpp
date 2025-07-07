e8b92ccc4ee1b3420043b803e04b1918e44451de
Rene David Charaña Jalire <darkreingreso@gmail.com>
2025-07-07 07:26:47 -0500

    Version final, Buscando un Valor en la Matriz(3x3)

--- ejercicio10.cpp
+++ ejercicio10.cpp
@@ -3,7 +3,7 @@
 #include <ctime>
 using namespace std;
 int main() {
-	cout << "Ejercicio 10: Generador de Tablero de Buscaminas\n";
+	cout << "Ejercicio 10: Generador de Tablero de Buscaminas en una Matriz(10x10)\n";
     srand(time(0));
     int m1[10][10];
     int m2[10][10];
@@ -11,7 +11,7 @@ int main() {
 
     for (int i=0; i<10; i++) {
         for (int j=0; j<10; j++) {
-            if (rand()%2== 0) {
+            if (rand()%5== 0) {
                 m1[i][j]=1; 
             } else {
                 m1[i][j]=0; 
@@ -36,7 +36,7 @@ int main() {
     }
 
     cout << "\n1) Tableros Generados:\n"<< endl;
-    cout << "       Original:\t\t\tNumeros lado a lado:\n\n";
+    cout << "  Original con minas:\t\t\tNumeros lado a lado:\n\n";
     for (int i = 0; i < 10; i++) {
     	cout << "  ";
         for (int j = 0; j < 10; j++) {
@@ -46,9 +46,7 @@ int main() {
                 cout << "0 ";
             }
         }
-
         cout << "\t\t\t";
-
         for (int j=0; j<10; j++) {
             cout << m2[i][j] << " ";
         }
